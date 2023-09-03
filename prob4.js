b142a06a9246a57ef11f560c2740032246d03f03
ShivayAnkit <byankitsharma@gmail.com>
2023-09-03 20:57:12 +0530

    git problem 4

---
+++ prob4.js
@@ -0,0 +1,7 @@
+let name = "Ankit";
+let schoolName = "MIT";
+let grade = "First";
+let rollNum = 140191;
+let section = "V";
+let marks = 98;
+console.log("Student Name" + name + "schoolName" + schoolName + " " + "rollNum" + rollNum + " " + "grade" + " " + grade);
